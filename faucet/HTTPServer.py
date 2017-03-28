bd5a07abeb5851c508f0e43d8d96f73311362a49
Michael Baird <michaelb_111@hotmail.com>
2017-03-28 20:08:51 +0000

    Captive portal rules now installed for clients that are idle. rules should only be inserted into config once.

--- faucet/HTTPServer.py
+++ faucet/HTTPServer.py
@@ -16,6 +16,30 @@ CAPFLOW = "/v1.1/authenticate/auth"
 AUTH_PATH = "/authenticate/auth"
 IDLE_PATH = "/idle"
 
+class Proto():
+    ETHER_ARP = 0x0806
+    ETHER_IPv4 = 0x0800
+    ETHER_IPv6 = 0x86DD
+    ETHER_EAPOL = 0x888E
+    IP_TCP = 6
+    IP_UDP = 17
+    DHCP_CLIENT_PORT = 68
+    DHCP_SERVER_PORT = 67
+    DNS_PORT = 53
+    HTTP_PORT = 80
+
+class HashableDict(dict):
+    '''
+        Copied from http://stackoverflow.com/a/1151686
+    '''
+    def __key(self):
+        return tuple((k,self[k]) for k in sorted(self))
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        return self.__key() == other.__key()
 
 class HTTPHandler(BaseHTTPRequestHandler):
     '''
@@ -35,6 +59,10 @@ class HTTPHandler(BaseHTTPRequestHandler):
     controller_pid_file = os.getenv('CONTR_PID', '/etc/ryu/contr_pid')
 
     faucet_config_file = os.getenv('FAUCET_CONFIG', '/home/ubuntu/faucet-dev/faucet.yaml') #'/etc/ryu/faucet/faucet.yaml')
+
+    gateway_ip = "10.0.5.2"
+    gateway_mac = "52:54:00:12:35:02"
+    portal_mac = "08:00:27:00:03:02"
 
     def _set_headers(self, code, ctype):
         self.send_response(code)
@@ -113,143 +141,113 @@ class HTTPHandler(BaseHTTPRequestHandler):
 
         return switch, port
 
-    def _get_captive_portal_acls(self, mac):
-
+    def _get_cp_arp_acls(self, mac):
+        """Creates two rules for allowing ARP requests from/to MAC
+        :param mac MAC address of the client
+        """
         # allow arp
         arpReq = dict()
-        arpReq["name"] = "captiveportal"
+        arpReq["name"] = "captiveportal_arp"
         arpReq["mac"] = mac
         arpReq["dl_src"] = DoubleQuotedScalarString(mac)
         arpReq["dl_type"] = Proto.ETHER_ARP
+        arpReq["arp_tpa"] = self.gateway_ip
         arpReq["actions"] = dict()
         arpReq["actions"]["allow"] = 1
+        arpReq["actions"]["dl_dst"] = self.gateway_mac
+
         areq = dict()
         areq["rule"] = arpReq
 
 
         arpReply = dict()
-        arpReply["name"] = "captiveportal"
+        arpReply["name"] = "captiveportal_arp"
         arpReply["mac"] = mac
-        arpReply["dl_dst"] = DoubleQuotedScalarString(mac)
+        arpReply["dl_src"] = DoubleQuotedScalarString(mac)
         arpReply["dl_type"] = Proto.ETHER_ARP
         arpReply["actions"] = dict()
         arpReply["actions"]["allow"] = 1
+        arpReply["actions"]["dl_dst"] = self.portal_mac
+
         arep = dict()
         arep["rule"] = arpReply
 
-        # allow dhcp
-        dhcpResp = dict()
-        dhcpResp["name"] = "captiveportal"
-        dhcpResp["mac"] = mac
-        dhcpResp["dl_dst"] = DoubleQuotedScalarString(mac)
-        dhcpResp["dl_type"] = Proto.ETHER_IP
-        dhcpResp["ip_proto"] = Proto.IP_UDP
-        dhcpResp["udp_src"] = Proto.DHCP_SERVER
-        dhcpResp["udp_dst"] = Proto.DHCP_CLIENT
-        dhcpResp["actions"] = dict()
-        dhcpResp["actions"]["allow"] = 1
-        dhres = dict()
-        dhres["rule"] = dhcpResp
+        return [areq, arep]
 
+    def _get_cp_dhcp_acls(self, mac):
+        # allow dhcp
         dhcpReply = dict()
-        dhcpReply["name"] = "captiveportal"
+        dhcpReply["name"] = "captiveportal_dhcp"
         dhcpReply["mac"] = mac
         dhcpReply["dl_src"] = DoubleQuotedScalarString(mac)
 #        dhcpReply["dl_dst"] = "ff:ff:ff:ff:ff:ff" # ignore for mean time.
 #   could also add ip.src 0.0.0.0 and dst 255.255.255.255 to resp and repl
-        dhcpReply["dl_type"] = Proto.ETHER_IP
+        dhcpReply["dl_type"] = Proto.ETHER_IPv4
         dhcpReply["ip_proto"] = Proto.IP_UDP
-        dhcpReply["udp_src"] = Proto.DHCP_CLIENT
-        dhcpReply["udp_dst"] = Proto.DHCP_SERVER
+#        dhcpReply["udp_src"] = Proto.DHCP_CLIENT
+        dhcpReply["udp_dst"] = Proto.DHCP_SERVER_PORT
         dhcpReply["actions"] = dict()
         dhcpReply["actions"]["allow"] = 1
+        # TODO could possibly rewrite MAC to DHCP server.
+
         dhrep = dict()
         dhrep["rule"] = dhcpReply
 
-        # allow dns
-        dnsResp = dict()
-        dnsResp["name"] = "captiveportal"
-        dnsResp["mac"] = mac
-        dnsResp["dl_dst"] = DoubleQuotedScalarString(mac)
-        dnsResp["dl_type"] = Proto.ETHER_IP
-        dnsResp["ip_proto"] = Proto.IP_UDP
-        dnsResp["udp_src"] = Proto.UDP_DNS
-#        dnsResp["udp_dst"] = #ignoring this for now
-        dnsResp["actions"] = dict()
-        dnsResp["actions"]["allow"] = 1
-        dnsres = dict()
-        dnsres["rule"] = dnsResp
+        return [dhrep]
 
+    def _get_cp_dns_acls(self, mac):
+        # allow dns
         dnsReply = dict()
-        dnsReply["name"] = "captiveportal"
+        dnsReply["name"] = "captiveportal_dns"
         dnsReply["mac"] = mac
         dnsReply["dl_src"] = DoubleQuotedScalarString(mac)
-        dnsReply["dl_type"] = Proto.ETHER_IP
+        dnsReply["dl_type"] = Proto.ETHER_IPv4
         dnsReply["ip_proto"] = Proto.IP_UDP
-        dnsReply["udp_dst"] = Proto.UDP_DNS
-#        dnsResp["udp_src"] = #ignoring this for now
+        dnsReply["udp_dst"] = Proto.DNS_PORT
         dnsReply["actions"] = dict()
         dnsReply["actions"]["allow"] = 1
+
         dnsrep = dict()
         dnsrep["rule"] = dnsReply
-        
-        # HTTP NAT
-        # match regardless of destination
-        # only worry about if its from a mac.
-        natFwd = dict()
-        natFwd["name"] = "captiveportal"
-        natFwd["mac"] = mac
-        natFwd["dl_src"] = DoubleQuotedScalarString(mac)
-        natFwd["dl_type"] = Proto.ETHER_IP
-        natFwd["ip_proto"] = Proto.IP_TCP
-#        natFwd["ipv4_src"] = 
-        natFwd["tcp_dst"] = Proto.TCP_HTTP
-#        natFwd["tcp_src"] = 
-        natFwd["actions"] = dict()
-        natFwd["actions"]["allow"] = 1
-        natFwd["actions"]["ipv4_dst"] = DoubleQuotedScalarString(PORTAL_WS_IP)
-        natFwd["actions"]["dl_dst"] = DoubleQuotedScalarString(PORTAL_WS_MAC)
-        natFwd["actions"]["output"] = dict()
-        natFwd["actions"]["output"]["controller"] = 1 # todo get faucet to support sending packets up to controller
 
-        natf = dict()
-        natf["rule"] = natFwd
+        return [dnsrep]
 
-        rules = [areq, arep, dhres, dhrep, dnsres, dnsrep, natFwd]
+    def _get_cp_tcp_acls(self, mac):
+        """
+        :param mac client's MAC address
+        :return Rule to redirect mac to a NFV portal by changing the dst MAC to the portal.
+        """
+        # TODO could possibly do a form of load balancing here,
+        #  by selecting a different portal dst mac address.
+        tcpFwd = dict()
+        tcpFwd["name"] = "captiveportal_tcp"
+        tcpFwd["mac"] = mac
+        tcpFwd["dl_src"] = DoubleQuotedScalarString(mac)
+        tcpFwd["dl_type"] = Proto.ETHER_IPv4
+        tcpFwd["ip_proto"] = Proto.IP_TCP
+        tcpFwd["tcp_dst"] = Proto.HTTP_PORT
+        tcpFwd["actions"] = dict()
+        tcpFwd["actions"]["allow"] = 1
+        tcpFwd["actions"]["dl_dst"] = self.portal_mac
+
+        ret = dict()
+        ret["rule"] = tcpFwd 
+
+        return [ret]
+
+    def _get_captive_portal_acls(self, mac):
+        """Generates the rules for mac to be able to use the captive portal.
+        :param mac MAC address of client
+        :return List of rules to be applied to a port ACL for the specified MAC address.
+        """
+        rules = []
+        rules.extend(self._get_cp_arp_acls(mac))
+        rules.extend(self._get_cp_dhcp_acls(mac))
+        rules.extend(self._get_cp_dns_acls(mac))
+        rules.extend(self._get_cp_tcp_acls(mac))
 
         return rules
 
-    def _get_cp_portal_nat_acl(self, client_mac, client_ip, foriegn_mac, foriegn_ip, tcp_dst):
-        """
-        returns the ACL to be applied on the port_acl for the portal, to perform the reverse NAT operation.
-        There will be one of these rules inserted for every tcp connection.
-        :param client_mac the MAC address of the host being redirected to the portal.
-        :param client_ip the IP address of the host.
-        :param foriegn_mac the MAC address of the next hop (probably a router) that the client is trying to access.
-        :param foriegn_ip the IP address that the client is trying to access.
-        :param tcp_dst the destination tcp port the client is connecting from. tcp_src (assumed to be 80). the names may seem backwards.
-        """
-        natRev = dict()
-        natRev["name"] = "captiveportal"
-        natRev["mac"] = client_mac
-        natRev["dl_src"] = DoubleQuotedScalarString(PORTAL_WS_MAC)
-        natRev["dl_dst"] = DoubleQuotedScalarString(client_mac)
-        natRev["dl_type"] = Proto.ETHER_IP
-        natRev["ip_proto"] = Proto.IP_TCP
-        natRev["ipv4_src"] = DoubleQuotedScalarString(PORTAL_WS_IP)
-        natRev["ipv4_dst"] = DoubleQuotedScalarString(client_ip)
-        natRev["tcp_src"] = Proto.TCP_HTTP
-        natRev["tcp_dst"] = tcp_dst
-        natRev["actions"] = dict()
-        natRev["actions"]["allow"] = 1
-        natRev["actions"]["ipv4_src"] = DoubleQuotedScalarString(foriegn_ip)
-        natRev["actions"]["dl_src"] = DoubleQuotedScalarString(foriegn_mac)
-
-        return [natRev]
-
-    def _add_cp_portal_nat_acl(self, cli):
-        pass
-    
     def _add_cp_acls(self, mac):
         """
         Adds the acls for captive portal redirection, to the port that mac is associated with.
@@ -292,16 +290,41 @@ class HTTPHandler(BaseHTTPRequestHandler):
             config["acls"][aclname] = updated_port_acl
         ruamel.yaml.round_trip_dump(config, open(self.faucet_config_file, 'w'), indent=4, block_seq_indent=4)
 
+    def _is_rule_in(self, rule, list_):
+        """Searches a list of HashableDicts for an item equal to rule.
+        :param rule an acl dict
+        :param list_ a list of HashableDicts
+        :return True if rule is is equal to item in list_, false otherwise
+        """
+        hash_rule = HashableDict(rule)
+        for item in list_:
+            if hash_rule == item:
+                return True
+        return False
+
+    def _get_hashable_list(self, list_):
+        """Creates a list of HashableDict for a list of dict.
+        :param list_ a list of dicts (standard python version)
+        :return a list of HashableDict
+        """
+        hash_list = []
+
+        for item in list_:
+            hash_list.append(HashableDict(item))
+
+        return hash_list
+
     def add_acls(self, mac, user, rules):
         """
         Adds the acls to a port acl that the mac address is associated with, in the faucet configuration file.
-        :param mac mac address of authenticated user
+        :param mac MAC address of authenticated user
         :param user username of authenticated user
         :param rules List of ACL rules to be applied to port that mac is associated with.
         """
         # get switchport
         switchname, switchport = self._get_switch_and_port(mac)
-	    # load faucet.yaml
+        # load faucet.yaml
+        # TODO what if the the config is split up accross multiple files?
         config, ind, bsi = load_yaml_guess_indent(open(self.faucet_config_file, 'r'))                
         print "switchname {0}, port {1}".format(switchname, switchport)
         aclname = config["dps"][switchname]["interfaces"][switchport]["acl_in"]
@@ -311,6 +334,9 @@ class HTTPHandler(BaseHTTPRequestHandler):
         # apply ACL for user to the switchport ACL
         new_port_acl = []
         inserted = False
+        #new_rules = set(rules).difference(port_acl)
+
+        hashable_port_acl = self._get_hashable_list(port_acl)
         for rule in port_acl:
             print rule
             #rule = rule["rule"]
@@ -322,20 +348,24 @@ class HTTPHandler(BaseHTTPRequestHandler):
                 if not inserted:
                     inserted = True
                     for new_rule in rules:
-                        print "new rule: " + str(rule)
-                        new_port_acl.insert(i, new_rule)
-                        i = i + 1
+                        if not self._is_rule_in(new_rule, hashable_port_acl):
+                            # only insert the new rule if it is not already in the port_acl (config file)
+                            #if rule is in rules
+                            print "new rule: " + str(new_rule)
+                            new_port_acl.insert(i, new_rule)
+                            i = i + 1
                 print "redir41x: " + str(rule)
                 new_port_acl.insert(i, rule)
                 i = i + 1
             else:
-                # insert new rule if not already
+                # insert new rule if not already.
                 if not inserted:
                     inserted = True
                     for new_rule in rules:
-                        print "new rule: " + str(rule)
-                        new_port_acl.insert(i, new_rule)
-                        i = i + 1
+                        if not self._is_rule_in(new_rule, hashable_port_acl):
+                            print "new rule: " + str(rule)
+                            new_port_acl.insert(i, new_rule)
+                            i = i + 1
                 print "un reck: " + str(rule)
                 new_port_acl.insert(i, rule)
                 i = i + 1
@@ -425,11 +455,8 @@ class HTTPHandler(BaseHTTPRequestHandler):
             mac = json_data["mac"]
             user = json_data["user"]
             rules = self.get_users_rules(mac, user)
-            self.add_acls(mac, user, rules)
-
             message = "authenticated new client({}) at MAC: {}\n".format(
                 user, mac)
-            self.send_signal(signal.SIGHUP)
 
         else:  #request is for CapFlow
             if not (json_data.has_key("ip") and json_data.has_key("user") and json_data.has_key("mac")):
@@ -439,18 +466,17 @@ class HTTPHandler(BaseHTTPRequestHandler):
             #valid request format so new user has authenticated
 #            self.write_to_file(self.capflow_file, json_data["ip"],
 #                               json_data["user"])
-#            self.send_signal(signal.SIGUSR2)
 
             mac = json_data["mac"]
             user = json_data["user"]
             ip = json_data["ip"]
             rules = self.get_users_rules(mac, user)
-            self.add_acls(mac, user, rules)
-
             message = "authenticated new client({}) at MAC: {} and ip: {}\n".format(
                 user, mac, ip)
-            self.send_signal(signal.SIGHUP)
- 
+
+        self.add_acls(mac, user, rules)
+        self.send_signal(signal.SIGHUP)
+
         #write response
         self._set_headers(200, 'text/html')
         self.wfile.write(message)
@@ -465,7 +491,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
                            json_data["retrans"])
 #        self.send_signal(signal.SIGUSR1)
         if json_data["retrans"] > 3:
-            self._add_cp_acl(json_data["mac"])
+            self._add_cp_acls(json_data["mac"])
             self.send_signal(signal.SIGHUP)
 
         self._set_headers(200, 'text/html')
