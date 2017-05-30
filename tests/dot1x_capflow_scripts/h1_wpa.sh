61985a14818714848591623923dc82456fcce29a
Michael Baird <michaelb_111@hotmail.com>
2017-05-30 18:29:07 +1200

    removed external script files

--- tests/dot1x_capflow_scripts/h1_wpa.sh
+++
@@ -1,3 +0,0 @@
-nohup wpa_supplicant -ih1-eth0 -Dwired -c/etc/wpa_supplicant/wpa_supplicant.conf > /dev/null 2>&1 & 
-
-
--- tests/dot1x_capflow_scripts/h2_wpa.sh
+++
@@ -1,3 +0,0 @@
-nohup wpa_supplicant -ih2-eth0 -Dwired -c/etc/wpa_supplicant/wpa_supplicant.conf > /dev/null 2>&1 & 
-
-
