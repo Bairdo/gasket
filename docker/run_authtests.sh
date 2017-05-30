ccca97caa06a023065a728e7554e15c08e85943b
Michael Baird <michaelb_111@hotmail.com>
2017-05-30 15:40:56 +1200

    updated running auth via docker. tests exit with error on fail

--- docker/run_authtests.sh
+++ docker/run_authtests.sh
@@ -4,7 +4,5 @@ service openvswitch-switch start
 
 cd /faucet-src/tests
 
-echo "=========== Running faucet unit tests ==========="
-time ./faucet_dotcap_integration.py FaucetIntegrationDot1XLogoffTest FaucetIntegrationDot1XLogonTest FaucetIntegrationNoLogOnTest
-echo "========== Running faucet config tests =========="
-./test_config.py
+echo "=========== Running faucet authentication unit tests ==========="
+time ./faucet_dotcap_integration.py FaucetIntegrationDot1XLogoffTest FaucetIntegrationDot1XLogonTest FaucetIntegrationNoLogOnTest || exit 1
