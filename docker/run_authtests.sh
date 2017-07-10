14f8e341c5728a7dc65b513fd2df967eff03d785
Michael Baird <michaelb_111@hotmail.com>
2017-07-10 21:28:54 +1200

    all tests pass

--- docker/run_authtests.sh
+++ docker/run_authtests.sh
@@ -4,6 +4,13 @@ service openvswitch-switch start
 
 cd /faucet-src/tests
 
-echo "=========== Running faucet authentication unit tests ==========="
-time ./faucet_mininet_test.py $FAUCET_TESTS FaucetAuthenticationDot1XLogoffTest FaucetAuthenticationDot1XLogonTest FaucetAuthenticationNoLogOnTest || exit 1
-# FaucetAuthenticationSomeLoggedOnTest
+echo "========== Running faucet config tests =========="
+python3 ./test_config.py || exit 1
+python3 ./test_check_config.py || exit 1
+
+echo "=========== Running faucet unit tests ==========="
+python3 ./test_valve.py || exit 1
+
+echo "=========== Running faucet system tests ==========="
+python2 ./faucet_mininet_test.py -c
+time ./faucet_mininet_test.py $FAUCET_TESTS || exit 1
