4d0ea8ac1d02420679e8e3b0832d6920de25b6e2
Michael Baird <michaelb_111@hotmail.com>
2017-07-11 19:08:18 +1200

    pylint - docstrings

--- faucet/my_lockfile.py
+++ faucet/my_lockfile.py
@@ -1,11 +1,24 @@
+"""Simple lockfile class used between faucet and the authentication controller app"""
+
 import fcntl
 import os
 
 def lock(lockfile, flags):
+    """Locks a interprocess file.
+    Args:
+        lockfile (str): name of file to lock.
+        flags: os flags to use when locking. e.g. os.O_RDWR
+             see https://docs.python.org/3/library/os.html#os.open for more
+    Returns:
+        filedescriptor of locked file.
+    """
     fd = os.open(lockfile, flags)
     fcntl.lockf(fd, fcntl.LOCK_EX)
     return fd
 
 def unlock(fd):
+    """unlocks a file descriptor
+    Args:
+        fd (file descriptor)"""
     fcntl.lockf(fd, fcntl.LOCK_UN)
     os.close(fd)
