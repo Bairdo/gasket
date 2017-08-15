74947994d6c12ce8f97e4fdcff097046718fbb45
Michael Baird <michaelb_111@hotmail.com>
2017-08-15 12:50:52 +1200

    docstring, tidy up

--- faucet/rule_manager.py
+++ faucet/rule_manager.py
@@ -1,3 +1,5 @@
+"""Handles the construction of the Faucet ACL configuration from the authentication application."""
+
 import logging
 import os
 import re
@@ -10,6 +12,9 @@ from rule_generator import RuleGenerator
 
 
 def main():
+    """Create a default base config and the initial Faucet ACL yaml file,
+    from a 'base' yaml file.
+    """
     # pylint: disable=unbalanced-tuple-unpacking
     input_f, output_f = sys.argv[1:3]
 
@@ -18,34 +23,35 @@ def main():
     # this is the original, (no one authenticated file). restore to this if necessary.
     shutil.copy2(input_f, input_f + '-orig')
 
+
 def create_base_faucet_acls(input_f, output_f):
-    '''
+    """
     Args:
         input_f (str): input filename (base config)
         output_f (str): output filename (faucet-acl.yaml)
-    '''
+    """
     with open(input_f) as f:
         base = yaml.safe_load(f)
     logging.basicConfig(filename='rule_man_base.log', level=logging.DEBUG)
     final = create_faucet_acls(base, logger=logging)
     write_yaml(final, output_f, True)
 
+
 def create_faucet_acls(doc, auth_rules=None, logger=None):
-    '''Creates a yaml object that represents faucet acls.
+    """Creates a yaml object that represents faucet acls.
     Args:
         doc (yaml object): yaml dict. containing the pre-faucet version of the
                             acls.
         auth_rules (dict): {aclname : list of rule} dictionary containing lists of faucet acl rules to insert. (Optional)
     Returns: yaml object {'acls': ...}
-    '''
+    """
     final = {}
     final['acls'] = {}
     final_acls = final['acls']
 
-    for acl in list(doc['acls'].items()):
+    for acl_name, acl in list(doc['acls'].items()):
         seq = []
-        acl_name = acl[0]
-        for obj in acl[1]:
+        for obj in acl:
             if isinstance(obj, dict) and 'rule' in obj:
                 # rule
                 for _, rule in list(obj.items()):
@@ -68,11 +74,6 @@ def create_faucet_acls(doc, auth_rules=None, logger=None):
                         seq.append(rule)
             elif isinstance(obj, list):
                 for y in obj:
-                    if isinstance(y, list):
-                        for z in y:
-                            if '_mac_' in z['rule']:
-                                del z['rule']['_mac_']
-                            seq.append(z)
                     if isinstance(y, dict):
                         # list of dicts
                         for _, rule in list(y.items()):
@@ -83,6 +84,10 @@ def create_faucet_acls(doc, auth_rules=None, logger=None):
                             if '_name_' in rule:
                                 del rule['_name_']
                             seq.append(new_rule)
+                    else:
+                        logger.warning('list of unrecognised objects')
+                        logger.warning('child type: %s' % type(y))
+                        logger.warning('list object: %s' % obj)
             elif isinstance(obj, str):
                 # this is likey just a 'flag' used to mark position to insert the rules when authed
                 if obj == 'authed-rules':
@@ -90,12 +95,22 @@ def create_faucet_acls(doc, auth_rules=None, logger=None):
                 else:
                     print(('illegal string ' + obj))
             else:
-                logger.warn('obj not reconised')
+                logger.warning('Object type %s not recognised' % type(obj))
+                logger.warning('Object: %s' % obj)
 
         final_acls[acl_name] = seq
     return final
 
+
 def write_yaml(yml, filename, ignore_aliases=False):
+    """Writes a yaml object to file.
+    Args:
+        yml (yaml): yaml object to write to file.
+        filename (str)
+        ignore_aliases (bool): True if yaml aliases should be removed
+                                and object written out in full.
+                                False if aliases can be used.
+    """
     if ignore_aliases:
         noalias_dumper = yaml.dumper.SafeDumper
         noalias_dumper.ignore_aliases = lambda self, data: True
@@ -106,15 +121,25 @@ def write_yaml(yml, filename, ignore_aliases=False):
             yaml.dump(yml, f, default_flow_style=False)
 
 
-
-
 class RuleManager(object):
+    """Handles the construction of the Faucet ACL configuration from the authentication
+    application.
+    """
 
+    def __init__(self, config):
+        self.config = config
+
+        self.rule_gen = RuleGenerator(self.config.rules)
+        self.base_filename = self.config.base_filename
+        self.faucet_acl_filename = self.config.acl_config_file
+        self.authed_users = {} # {mike: {aa:aa:aa:aa:aa:aa: {faucet-1: {p1: 1. p2: 1}}}}
+    
     def add_to_base_acls(self, filename, rules, user, logger=None):
         '''Adds rules to the base acl file (and writes).
         Args:
             filename (str);
             rules (dict): {port_s1_1 : list of rules}
+            user (str): username
         '''
         with open(filename) as f:
             base = yaml.safe_load(f)
@@ -144,25 +169,35 @@ class RuleManager(object):
         return base
 
     def authenticate(self, username, mac, switch, port, radius_fields=None, logger=None):
+        """Authenticates a username and MAC address on a switch and port.
+        Args:
+            username (str)
+            mac (str): MAC address
+            switch (str): Switch that authentication occured on
+            port (str): the 'access port' as configured in 'auth.yaml'
+        """
         # get rules to apply
-        try:
-            if not self.is_authenticated(mac, username, switch, port):
-                self.add_to_authed_dict(username, mac, switch, port)
-                rules = self.rule_gen.get_rules(username, 'port_' + switch + '_' + str(port), mac)
-                # update base
-                base = self.add_to_base_acls(self.base_filename, rules, username, logger=logger)
-                # update faucet
-                final = create_faucet_acls(base, logger=logger)
-                write_yaml(final, self.faucet_acl_filename + '.tmp' , True)
-                self.backup_file(self.faucet_acl_filename)
-                self.swap_temp_file(self.faucet_acl_filename)
-                # sighup.
-                self.send_signal(signal.SIGHUP)
-        except Exception as e:
-            logger.critical('except while authenticate')
-            logger.exception(e)
+        if not self.is_authenticated(mac, username, switch, port):
+            self.add_to_authed_dict(username, mac, switch, port)
+            rules = self.rule_gen.get_rules(username, 'port_' + switch + '_' + str(port), mac)
+            # update base
+            base = self.add_to_base_acls(self.base_filename, rules, username, logger=logger)
+            # update faucet
+            final = create_faucet_acls(base, logger=logger)
+            write_yaml(final, self.faucet_acl_filename + '.tmp' , True)
+            self.backup_file(self.faucet_acl_filename)
+            self.swap_temp_file(self.faucet_acl_filename)
+            # sighup.
+            self.send_signal(signal.SIGHUP)
 
     def remove_from_base(self, username, mac, logger=None):
+        """Removes rules that have matching mac= _mac_ and username=_name_
+        If both _mac_ and _name_ exist in the rule, both must match
+        If only one of _mac_ or _name_ is exist, only one must match.
+        Args:
+            username (str)
+            mac (str): MAC address
+        """
         with open(self.base_filename) as f:
             base = yaml.safe_load(f)
        
@@ -206,33 +241,30 @@ class RuleManager(object):
         return base, removed
 
     def deauthenticate(self, username, mac, logger=None):
-        try:
-            if self.is_authenticated(mac, username):
-                self.remove_from_authed_dict(username, mac)
-                # update base
-                base, changed = self.remove_from_base(username, mac, logger=logger)
-                # update faucet only if config has changed
-                if changed:
-                    final = create_faucet_acls(base, logger=logger)
-                    write_yaml(final, self.faucet_acl_filename + '.tmp', True)
+        """Deauthenticates a username or MAC address.
+        Args:
+            username (str): may be None or '(null)' which is treated as None.
+            mac (str): MAC address
+        """
+        if self.is_authenticated(mac, username):
+            self.remove_from_authed_dict(username, mac)
+            # update base
+            base, changed = self.remove_from_base(username, mac, logger=logger)
+            # update faucet only if config has changed
+            if changed:
+                final = create_faucet_acls(base, logger=logger)
+                write_yaml(final, self.faucet_acl_filename + '.tmp', True)
 
-                    self.backup_file(self.faucet_acl_filename)
-                    self.swap_temp_file(self.faucet_acl_filename)
-                    # sighup.
-                    self.send_signal(signal.SIGHUP)
-        except Exception as e:
-            logger.critical('except while authenticate')
-            logger.exception(e)
-
-    def __init__(self, config):
-        self.config = config
-
-        self.rule_gen = RuleGenerator(self.config.rules)
-        self.base_filename = self.config.base_filename
-        self.faucet_acl_filename = self.config.acl_config_file
-        self.authed_users = {} # {mike: {aa:aa:aa:aa:aa:aa: {faucet-1: {p1: 1. p2: 1}}}}
+                self.backup_file(self.faucet_acl_filename)
+                self.swap_temp_file(self.faucet_acl_filename)
+                # sighup.
+                self.send_signal(signal.SIGHUP)
 
     def backup_file(self, filename):
+        """Backup a file. appends '.bak#' to filename.
+        Args:
+            filename (str)
+        """
         directory = os.path.dirname(filename)
         if directory == '':
             directory = '.'
@@ -247,9 +279,10 @@ class RuleManager(object):
         # backup old current
         shutil.copy2(filename, filename + '.bak' + i)
 
-
     def swap_temp_file(self, filename):
         """Renames the temporary file to become the original.
+        Args:
+            filename (str)
         """
         os.remove(filename)
         # make new tmp the current.
@@ -267,10 +300,10 @@ class RuleManager(object):
     def is_authenticated(self, mac, username=None, switch=None, port=None):
         '''Checks if a username is already authenticated with the MAC address on the switch & port.
         Args:
-            username (str)
+            username (str): optional
             mac (str)
-            switch (str)
-            port (str)
+            switch (str): optional
+            port (str): optional. if switch is used so should port and vice versa.
         Returns:
             True if already authenticated. False otherwise.
         '''
@@ -288,6 +321,13 @@ class RuleManager(object):
         return False
 
     def add_to_authed_dict(self, username, mac, switch, port):
+        """Add an authenticted user, ... to the authed_users dictionary
+        Args:
+            username (str)
+            mac (str): MAC address.
+            switch (str): the name of the switch username has authenticated on.
+            port (str): the port the username has authenticated on.
+        """
         if username not in self.authed_users:
             self.authed_users[username] = {}
             self.authed_users[username][mac] = {}
@@ -306,8 +346,14 @@ class RuleManager(object):
                     if port not in self.authed_users[username][mac][switch]:
                         self.authed_users[username][mac][switch][port] = 1
 
-
     def remove_from_authed_dict(self, username, mac):
+        """Remove the mac from the authed_users dictionary.
+        If username is None or '(null)' as is the case with some deauthentications,
+        the mac is removed from all users.
+        Args:
+            username (str): may be None or '(null)'.
+            mac (str): MAC address,
+        """
         if username and username != '(null)':
             if username in self.authed_users:
                 del self.authed_users[username][mac]
@@ -319,22 +365,6 @@ class RuleManager(object):
             for user in remove_users:
                 del self.authed_users[user][mac]
 
-def main2():
-    base_filename = 'acls.yaml'
-    faucet_acl_filename = 'faucet-acl.yaml'
-    rule_filename = 'vm-net/rules.yaml'
-#    rule_man = RuleManager(rule_filename, base_filename, faucet_acl_filename)
-
-
-#   rule_man.authenticate('host110user', '33:33:33:33:33:33', 'faucet-1', 1)
-#   rule_man.deauthenticate('host110user', '33:33:33:33:33:33')
 
 if __name__ == '__main__':
     main()
-
-
-# part 1:
-#   convert acls.yaml to faucet-acls.yaml
-# part 2:
-# 
-
