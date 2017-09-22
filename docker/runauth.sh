beda51a0dffbe8cbe984078b8d2b8d5a2c04b2e1
Michael Baird <michaelb_111@hotmail.com>
2017-09-22 12:51:56 +1200

    get auth dockerfile working

--- docker/runauth.sh
+++ docker/runauth.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-cd /root/hostapd-2.6/hostapd 
-cp /etc/hostapd/hostapd.config .config
-make && make install
-hostapd -dd /etc/hostapd/wired.conf > /etc/hostapd/hostapd.log 2>&1 &
-#freeradius -X > /var/log/ryu/faucet/freerad.log 2>&1 &
+hostapd -ddt /etc/hostapd/wired.conf > /etc/hostapd/hostapd.log 2>&1 &
 python3 /faucet-src/faucet/rule_manager.py /etc/ryu/faucet/base-acls.yaml /etc/ryu/faucet/faucet-acls.yaml
 ryu-manager faucet.faucet &
 echo $! > /etc/ryu/faucet/contr_pid
