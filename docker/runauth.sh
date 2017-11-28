2ef0331f6e9e02a3bd010b2384edca484a41a26d
Michael Baird <michaelb_111@hotmail.com>
2017-11-28 13:55:20 +1300

    pip install gasket

--- docker/runauth.sh
+++ docker/runauth.sh
@@ -3,4 +3,4 @@ echo "Using base-no-authed-acls.yaml where no users are authenticated."
 cp /etc/ryu/faucet/gasket/base-no-authed-acls.yaml /etc/ryu/faucet/gasket/base-acls.yaml
 python3 /gasket-src/gasket/rule_manager.py /etc/ryu/faucet/gasket/base-acls.yaml /etc/ryu/faucet/faucet-acls.yaml
 echo "Starting Faucet and Gasket"
-ryu-manager --verbose --pid-file=/var/run/faucet.pid faucet.faucet /gasket-src/gasket/auth_app.py
+ryu-manager --pid-file=/var/run/faucet.pid faucet.faucet gasket.auth_app
---
+++ setup.py
@@ -0,0 +1,51 @@
+import errno
+import os
+import shutil
+import sys
+
+from pkg_resources import resource_filename
+from setuptools import setup, find_packages
+
+def install_configs():
+    """Install configuration files to /etc/ryu/"""
+    dst_faucet_conf_dir = '/etc/ryu/faucet/'
+    dst_gasket_conf_dir = '/etc/ryu/faucet/gasket/'
+
+    src_faucet_conf_dir = resource_filename(__name__, 'etc/ryu/faucet')
+    src_gasket_conf_dir = resource_filename(__name__, 'etc/ryu/faucet/gasket')
+    gasket_log_dir = '/var/log/ryu/faucet/gasket/'
+
+    try:
+        if not os.path.exists(dst_faucet_conf_dir):
+            print ("Creating %s" % dst_faucet_conf_dir)
+            os.makedirs(dst_faucet_conf_dir)
+        if not os.path.exists(dst_gasket_conf_dir):
+            print ("Creating %s" % dst_gasket_conf_dir)
+            os.makedirs(dst_gasket_conf_dir)
+        for src_dir, dst_dir in [(src_faucet_conf_dir, dst_faucet_conf_dir), (src_gasket_conf_dir, dst_gasket_conf_dir)]:
+            for filename in os.listdir(src_dir):
+                src_file = os.path.join(src_dir, filename)
+                dst_file = os.path.join(dst_dir, filename)
+                if os.path.isfile(src_file) and not os.path.isfile(dst_file):
+                    print('Copying %s to %s' % (src_file, dst_file))
+                    shutil.copy(src_file, dst_file)
+        if not os.path.exists(gasket_log_dir):
+            print("Creating %s" % gasket_log_dir)
+            os.makedirs(gasket_log_dir)
+    except OSError as ex:
+        if ex.errno == errno.EACCES:
+            print('Permission denied creating %s, skipping ocpying configs' % ex.filename)
+        else:
+            raise
+
+
+
+setup(
+    name='gasket',
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pby=True,
+    packages=find_packages()
+)
+
+if 'install' in sys.argv or 'bdist_wheel' in sys.argv:
+    install_configs()
