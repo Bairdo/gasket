87cacea19ed2951c3c662f16ae126f5e16cc5f19
Michael Baird <michaelb_111@hotmail.com>
2018-04-06 11:49:24 +1200

    updated prometheus thread docs

--- gasket/prometheus_thread.py
+++ gasket/prometheus_thread.py
@@ -12,7 +12,14 @@ LEARNED_MACS_REGEX = r"""learned_macs{dp_id="(0x[a-f0-9]+)",dp_name="([\w-]+)",n
 
 
 class Prometheus(threading.Thread):
-    """Thread that periodically queries Faucet's prometheus, for mac learning."""
+    """Thread that periodically queries Faucet's Prometheus, for mac learning.
+    The consequence of missing an L2Learn event from rabbitmq is that the host will not be learnt until another event is sent.
+    Which may not happen for several minutes (and therefore authentication cannot fully complete).
+    
+    There is the possibility that we'll receive two events for the same learning (one from prometheus & one from rabbitmq).
+    If the events are the same (same port etc), it does not matter.
+    If the events are different (different port), the order of learning may get wacky.
+    """
 
     work_queue = None
     logger = None
@@ -40,18 +47,18 @@ class Prometheus(threading.Thread):
             time.sleep(self.sleep_period)
 
     def get_prometheus_mac_learning(self):
-        """Queries the prometheus faucet client,
+        """Queries the Prometheus faucet client,
         And creates L2Learn work for macs already learnt.
         """
         # query faucets promethues.
-        self.logger.info('querying prometheus for "learned_macs"')
+        self.logger.info('querying Prometheus for "learned_macs"')
         try:
             prom_mac_table = auth_app_utils.scrape_prometheus_vars(self.server_url,
                                                                    ['learned_macs'])[0]
         except Exception as e:
             self.logger.exception(e)
             return
-        self.logger.debug('queried prometheus. mac_table:\n%s\n',
+        self.logger.debug('queried Prometheus. mac_table:\n%s\n',
                           prom_mac_table)
 
         for line in prom_mac_table:
@@ -59,8 +66,7 @@ class Prometheus(threading.Thread):
             macstr = auth_app_utils.float_to_mac(float_as_mac)
             self.logger.debug('float %s is mac %s', float_as_mac, macstr)
 
-            # if this is also an access port, we have found the dpid and the port
             values = self.learned_macs_compiled_regex.match(labels)
             dpid, dp_name, n, port, vlan = values.groups()
-            self.work_queue.put(work_item.L2LearnWorkItem(dp_name, int(dpid, 16), int(port), int(vlan), macstr, None))
-
+            self.work_queue.put(work_item.L2LearnWorkItem(dp_name, int(dpid, 16),
+                                                          int(port), int(vlan), macstr, None))
